4c3be981fd6ce441bfb561ccfa5fe1134b7c7325
Daniel Camargo <pererinha@gmail.com>
2011-04-10 20:39:44 -0300

    First commit ;)

---
+++ ApacheLogToMysql.py
@@ -0,0 +1,59 @@
+import MySQLdb 
+import time, datetime
+
+class dateTime():
+  def __init__(self):
+    self
+
+  def convert(self, stringDate):
+    date = time.strptime(stringDate, "%d/%b/%Y:%H:%M:%S")
+    return time.strftime("%Y-%m-%d %H:%M:%S", date)
+    self.name = name
+
+class ApacheLog2Mysql:
+ def __init__(self):
+  
+  try:
+	# database configuration
+    con = MySQLdb.connect('<address>', '<user>', '<password>')
+    con.select_db('<database name>')
+    cursor = con.cursor() 
+
+  except MySQLdbError, error:
+    print(str(error), "Info")
+    exit()
+  # path to log files
+  serverLogs = ('<log-file-1>','<log-file-2>', <...>)
+  dt = dateTime()
+  for log in serverLogs:
+    numLog = len(open(log).readlines())
+    
+    
+    print log, numLog
+    for line in open(log, "r"):
+      data = []
+      a = line.split('"')
+      line = line.split()
+      data.append(line[0]) #ip 0
+      dataLine = line[3][1:line[3].index(":")] + ':' + line[3][line[3].index(":") + 1:]
+      data.append(dt.convert(dataLine)) #datetime 1
+      data.append(line[4][:-1]) #gmt 2
+      data.append(line[5][1:] + " " + line[6]) #urlreq 3
+      data.append(line[8]) #statuscode 4
+      data.append(line[9]) #bytestr 5
+      data.append(line[10][1:-1]) #referel 6
+      data.append(a[-2]) #osagent 7
+
+      try:
+        cursor.execute("insert into logapache (ip, date, gmt, request, errorcode, bytes, referel, osa) values (%s, %s, %s, %s, %s, %s, %s, %s)", (data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7]))
+        con.commit()
+
+      except MySQLdb.Error, error:
+        print(str(error), 'Info')
+        exit()
+
+  
+  cursor.close()
+  con.close()
+
+ap = ApacheLog2Mysql()
